ef7dd9f12d0a334dedef768150c06b5c8563d007
Hai.Tran <tranhaicoder@gmail.com>
2019-12-03 16:13:33 +0700

    update file printShape

--- HaiTran_Ss1/Baitap/printShape/index.php
+++ HaiTran_Ss1/Baitap/printShape/index.php
@@ -1,3 +1,50 @@
+<?php
+function printRectangle() {
+    $draw = '';
+    for($i = 0; $i < 3; $i++) {
+        for ($j = 0; $j < 5; $j++)
+            $draw .= ' * ';
+        $draw .= "<br>";
+    }
+    return $draw;
+}
+
+function printSquareTriangle() {
+    $draw = '';
+    for($i = 0; $i < 5; $i++) {
+        for ($j = 0; $j <= $i; $j++)
+            $draw .= ' * ';
+        $draw .= "<br>";
+    }
+    return $draw;
+}
+
+function printIsosscelesTriangle() {
+    $draw = '';
+    for($i = 0; $i < 5; $i++) {
+        for ($j = 5; $j >= $i; $j--)
+            $draw .= ' * ';
+        $draw .= "<br>";
+    }
+    return $draw.'*';
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    $valueSelect = $_POST["selectShape"];
+    switch ($valueSelect) {
+        case 1:
+            $printShape = printRectangle();
+            break;
+        case 2:
+            $printShape = printSquareTriangle();
+            break;
+        case 3:
+            $printShape = printIsosscelesTriangle();
+    }
+}
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -10,12 +57,23 @@
 <body>
 <div>
     <form action="" method="post">
-        <h1>Menu</h1>
-        <label><input type="radio" name="draw">1. Print the rectangle.</label>
-        <label><input type="radio" name="draw">1. Print the triangle.</label>
-        <label><input type="radio" name="draw">1. Print the rectangle.</label>
-        <label><input type="radio" name="draw">1. Print the rectangle.</label>
+        <fieldset>
+            <legend>Menu</legend>
+            Choose your selection:
+            <select name="selectShape" id="">
+                <option value="1">1. Print the rectangle.</option>
+                <option value="2">2. Print the square triangle.</option>
+                <option value="3">3. Print isosceles triangle.</option>
+                <option value="4">4. Exit.</option>
+            </select>
+            <input type="submit" value="Submit">
+        </fieldset>
     </form>
+</div>
+<div>
+    <?php
+        echo $printShape;
+    ?>
 </div>
 </body>
 </html>
